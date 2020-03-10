cde6e0d53c1314b91ce8aae4addb0dc6de5ee9b2
sleipold <simon.leipold@gmx.de>
2020-03-10 20:30:30 +0100

    init

---
+++ lib/viewmodels/login_view_model.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/foundation.dart';
+import 'package:showwallet/constants/route_names.dart';
+import 'package:showwallet/locator.dart';
+import 'package:showwallet/services/authentication_service.dart';
+import 'package:showwallet/services/dialog_service.dart';
+import 'package:showwallet/services/navigation_service.dart';
+
+import 'base_model.dart';
+
+class LoginViewModel extends BaseModel {
+  final AuthenticationService _authenticationService =
+  locator<AuthenticationService>();
+  final DialogService _dialogService = locator<DialogService>();
+  final NavigationService _navigationService = locator<NavigationService>();
+
+  Future login({@required String email, @required String password}) async {
+    setBusy(true);
+
+    var result = await _authenticationService.loginWithEmail(
+        email: email, password: password);
+
+    setBusy(false);
+
+    if (result is bool) {
+      if (result) {
+        _navigationService.navigateTo(HomeViewRoute);
+      } else {
+        await _dialogService.showDialog(
+          title: 'Login Failure',
+          description: 'Couldn\'t login at this moment. Please try again later',
+        );
+      }
+    } else {
+      await _dialogService.showDialog(
+        title: 'Login Failure',
+        description: result,
+      );
+    }
+  }
+}
---
+++ lib/viewmodels/signup_view_model.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/foundation.dart';
+import 'package:showwallet/constants/route_names.dart';
+import 'package:showwallet/locator.dart';
+import 'package:showwallet/services/authentication_service.dart';
+import 'package:showwallet/services/dialog_service.dart';
+import 'package:showwallet/services/navigation_service.dart';
+
+import 'base_model.dart';
+
+class SignUpViewModel extends BaseModel {
+  final AuthenticationService _authenticationService =
+  locator<AuthenticationService>();
+  final DialogService _dialogService = locator<DialogService>();
+  final NavigationService _navigationService = locator<NavigationService>();
+
+  Future signUp({@required String email, @required String password}) async {
+    setBusy(true);
+
+    var result = await _authenticationService.signUpWithEmail(
+        email: email, password: password);
+
+    setBusy(false);
+    if (result is bool) {
+      if (result) {
+        _navigationService.navigateTo(HomeViewRoute);
+      } else {
+        await _dialogService.showDialog(
+          title: 'Sign Up Failure',
+          description: 'General sign up failure. Please try again later',
+        );
+
+      }
+    } else {
+      await _dialogService.showDialog(
+        title: 'Sign Up Failure',
+        description: result,
+      );
+    }
+  }
+}
